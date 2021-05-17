13c0513557b30565b58a60f9b5bbe9fcef7e3fb7
dfm88 <diegof.university@gmail.com>
2021-05-17 23:37:05 +0200

    Create Form, next Update Form

--- djangocrm/djangocrm/urls.py
+++ djangocrm/djangocrm/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
-from leads.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page)
+    path('leads/', include('leads.urls', namespace="leads"))
 ]
--- djangocrm/leads/views.py
+++ djangocrm/leads/views.py
@@ -1,9 +1,54 @@
-from django.shortcuts import render
+from leads.models import Lead, Agent
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from leads.form import LeadForm, LeadModelForm
 
 # Create your views here.
 
 
-def home_page(request):
-    # return HttpResponse("Hello World")
-    return render(request, "leads/home_page.html")
+def lead_list(request):
+
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
+
+
+def lead_create(request):
+    form = LeadModelForm()  # Create an empty form
+
+    # check if it'is a POST method
+    if request.method == 'POST':
+        print('Receiving a POST Request')
+        # validating and repopulate the Form
+        form = LeadModelForm(request.POST)
+        # validate the form
+        if form.is_valid():
+            form.save()
+            # Controlli non necessari se si usa un Model Form,
+            # basta il metodo form.save()
+            """  print(form.cleaned_data)  # Print Cleaned Data
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = form.cleaned_data['agent']
+            # Create the Lead into DB
+            Lead.objects.create(first_name=first_name,
+                                last_name=last_name, age=age,
+                                agent=agent) """
+            # redirect to Lead List
+            return redirect('/leads')
+
+    context = {
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context)
